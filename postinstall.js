c77b05f7316ec534b63f28a6b3286ef3092c2d98
Bruno Parga <brunoparga@gmail.com>
2025-01-30 15:08:58 +0200

    Print post-install instructions

--- postinstall.js
+++ postinstall.js
@@ -1,5 +1,5 @@
-console.error('=================================================================');
-console.error('        Mercury SDK requires a Mercury API key to be set.');
-console.error('=================================================================');
-console.error('If you have one, set it to the MERCURY_API_KEY environment variable.');
-console.error('Otherwise, run: npx mercury-setup\n');
+process.stderr.write('=================================================================');
+process.stderr.write('        Mercury SDK requires a Mercury API key to be set.');
+process.stderr.write('=================================================================');
+process.stderr.write('If you have one, set it to the MERCURY_API_KEY environment variable.');
+process.stderr.write('Otherwise, run: npx mercury-setup\n');
